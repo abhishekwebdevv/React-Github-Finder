979af483b2df794305d06a45b1f4d8b8fe01fba8
sageOMEGA <abhishekdhakad61@gmail.com>
2020-05-29 00:27:53 +0530

    added NotFound Page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Alert from "./components/layouts/Alert";
 import User from "./components/users/User";
 import About from "./components/pages/About";
 import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 
 import GithubState from "./components/context/github/GithubState";
 import AlertState from "./components/context/alert/AlertState";
@@ -23,6 +24,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/users/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
